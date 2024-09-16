0e66bbb1ab946e0331b991385694b1523cc78348
Li Linchao <colin1@foxmail.com>
2024-09-16 21:42:38 +0800

    增强 toc 组件的功能

--- scripts/tags/lib/toc.js
+++ scripts/tags/lib/toc.js
@@ -7,13 +7,35 @@
 
 'use strict'
 
-function layoutDocTree(ctx, pages) {
+function layoutDocTree(ctx, pages, wikiName) {
   const url_for = require('hexo-util').url_for.bind(ctx)
+  // 辅助函数：确保 URL 正确拼接
+  function joinUrl(base, path) {
+    if (base.endsWith('/') && path.startsWith('/')) {
+      return base + path.slice(1)
+    } else if (!base.endsWith('/') && !path.startsWith('/')) {
+      return base + '/' + path
+    } else {
+      return base + path
+    }
+  }
+
   var el = ''
   el += '<ul class="toc">'
   pages.forEach((p, i) => {
+    let href
+    if (p.path.startsWith(wikiName + '/')) {
+      // 如果路径已经以 wikiName 开头，直接使用
+      href = url_for(p.path)
+    } else {
+      // 否则，添加 wikiName 前缀
+      href = url_for(wikiName + '/' + p.path)
+    }
+    // 使用 ctx.config.url 作为基础 URL
+    href = joinUrl(ctx.config.url, href)
+    // console.log("href", href)
     el += '<li>'
-    el += '<a class="list-link" href="' + url_for(p.path) + '">'
+    el += '<a class="list-link" href="' + href + '">'
     el += '<span>' + p.title + '</span>'
     el += '</a>'
     el += '</li>'
@@ -54,14 +76,14 @@ module.exports = ctx => function(args) {
         el += '<div class="header">'
         el += sec.title
         el += '</div>'
-        el += layoutDocTree(ctx, sec.pages)
+        el += layoutDocTree(ctx, sec.pages, args.wiki)
         el += '</section>'
       })
       el += '</div>'
     } else {
       el += '<div class="body fs14">'
       el += '<div class="body">'
-      el += layoutDocTree(ctx, proj.pages)
+      el += layoutDocTree(ctx, proj.pages, args.wiki)
       el += '</div>'
       el += '</div>'
     }
